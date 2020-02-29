a8e0ed3d4f382a83dad77a6746fec07f32fe7f89
Igor T Belem <igortbelem@gmail.com>
2020-02-29 11:15:50 -0500

    aganeee

---
+++ backend/routes/persons.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const axios = require('axios')
+
+router.route('/getperson/:id').get((req, res) => {
+    axios.get(`https://swapi.co/api/people/${req.params.id}`,{
+      }).then((response) =>{
+          console.log(response.data);
+          res.json(response.data)
+          
+      }, (error) =>{
+          console.log(error);
+      })
+});
+
+router.route('/search/:id').get((req, res) => {
+    axios.get(`https://swapi.co/api/people/?search=${req.params.id}`,{
+      }).then((response) =>{
+          res.json(response.data)
+          
+      }, (error) =>{
+          console.log(error);
+      })
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+
+const personsRouter = require('./routes/persons');
+
+app.use('/', personsRouter);
+
+app.listen(5000, () =>{
+    console.log('Listening on port 5000...')
+})
