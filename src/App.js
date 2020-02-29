b7ba21004c85c0dfbe0e33bb27e8136d25a704e1
Igor T Belem <igortbelem@gmail.com>
2020-02-29 11:06:25 -0500

    again?

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table, Form } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup'
+import Card from 'react-bootstrap/Card'
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      persons:[],
+      value: '',
+      name:'',
+      height:'',
+      weight:''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-export default App;
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.pullPersonSearch(this.state.value);
+    event.preventDefault();
+  }
+
+  componentDidMount() {
+  }
+  
+  pullPerson = (num) =>{
+    axios.get(`http://localhost:5000/getperson/${num}`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+        console.log(this.state.persons)
+      })
+    setTimeout(() => {}, 5000) // rate limiting
+  }
+
+  pullPersonSearch = (str) =>{
+    axios.get(`http://localhost:5000/search/${str}`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+        if (this.state.persons.results[0] !== undefined)
+        {
+          this.setState({name: this.state.persons.results[0]['name']});
+          this.setState({weight: this.state.persons.results[0]['mass']});
+          this.setState({height: this.state.persons.results[0]['height']});
+        }
+      })
+    setTimeout(() => {}, 5000) // rate limiting
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formSearch"> 
+            <Form.Control type="text" value={this.state.value} onChange={this.handleChange}placeholder="Search" />
+          </Form.Group>
+        </Form>
+        <Table striped bordered hover className="text-center">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Height</th>
+              <th>Weight</th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>{this.state.name}</td>
+            <td>{this.state.height}</td>
+            <td>{this.state.weight}</td>
+          </tr>
+          </tbody>
+        </Table>
+        
+      </div>
+    );
+  }
+}
