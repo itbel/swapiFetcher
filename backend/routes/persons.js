6e15f70c193a1380984f451113ac978b9c1a95d1
Igor Belem <igortbelem@gmail.com>
2020-04-15 13:53:00 -0400

    fixed api url

--- backend/routes/persons.js
+++ backend/routes/persons.js
@@ -1,25 +1,27 @@
-const router = require('express').Router();
-const axios = require('axios')
+const router = require("express").Router();
+const axios = require("axios");
 
-router.route('/getperson/:id').get((req, res) => {
-    axios.get(`https://swapi.co/api/people/${req.params.id}`,{
-      }).then((response) =>{
-          console.log(response.data);
-          res.json(response.data)
-          
-      }, (error) =>{
-          console.log(error);
-      })
+router.route("/getperson/:id").get((req, res) => {
+  axios.get(`https://swapi.dev/api/people/${req.params.id}`, {}).then(
+    (response) => {
+      console.log(response.data);
+      res.json(response.data);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 });
 
-router.route('/search/:id').get((req, res) => {
-    axios.get(`https://swapi.co/api/people/?search=${req.params.id}`,{
-      }).then((response) =>{
-          res.json(response.data)
-          
-      }, (error) =>{
-          console.log(error);
-      })
+router.route("/search/:id").get((req, res) => {
+  axios.get(`https://swapi.dev/api/people/?search=${req.params.id}`, {}).then(
+    (response) => {
+      res.json(response.data);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 });
 
 module.exports = router;
